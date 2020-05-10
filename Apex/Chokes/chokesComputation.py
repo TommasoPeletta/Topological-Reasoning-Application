5f830e4cfe841ad022cc165d5301c65ba0f78e74
TommasoPeletta <tommaso.peletta@etu.unige.ch>
2020-05-10 17:14:40 +0200

    chokes segmentation

--- Apex/Chokes/chokesComputation.py
+++ Apex/Chokes/chokesComputation.py
@@ -1,11 +1,13 @@
 import subprocess
+import os
+import matplotlib.image as mpimg
+import numpy as np
 
 k = 0
 
-for i in range(2):
-    for j in range(2):
+for i in range(57):
+    for j in range(57):
         if (i < j):
-            k += 1
             voxfile = open("mountChokes.imgql", "w")
             voxfile.write('''import "stdlib.imgql"
 // filter function definition
@@ -23,13 +25,21 @@ let r2 = red(image2)
 let treshold1 = r1 >. 100
 let treshold2 = r2 >. 100
 
-let zone1 = distleq(40, treshold1)
-let zone2 = distleq(40, treshold2)
+let zone1 = distleq(3, treshold1)
+let zone2 = distleq(3, treshold2)
 
 let union = zone1 & zone2 & !treshold1 & !treshold2
+let union1 = touch(union, treshold1) & touch(union, treshold2)
 
-
-save "./output/union''' +str(k)+'''.png" union''')
+save "./output/union''' +str(k)+'''.png" union1''')
+            print(i)
             voxfile.close()
-            sp = subprocess.Popen(["/bin/bash", "-i", "-c", "voxlogica mountChokes.imgql"])
+            #os.system('source /home/tommyubuntu/.bashrc; voxlogica mountChokes.imgql')
+            sp = subprocess.Popen(["/home/tommyubuntu/Documenti/VoxLogicA_0.5.5_linux-x64/VoxLogicA", "mountChokes.imgql"])
             sp.communicate()
+            img = mpimg.imread('./output/union'+str(k)+'.png')
+            im_array = np.array(img, dtype='uint8')
+            if (np.sum(im_array) == 0):
+                os.system('rm ./output/union'+str(k)+'.png')
+            else:
+                k += 1
